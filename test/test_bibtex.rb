de06296230e3121226f8142b1c08af12fd8484ae
Sylvester Keil <sk@semicolon.at>
2010-08-03 11:41:22 +0200

    Updated to new Bibliography syntax

--- test/test_bibtex.rb
+++ test/test_bibtex.rb
@@ -24,14 +24,15 @@ class TestBibtex < MiniTest::Unit::TestCase
     bib = @parser.parse(File.open('test/bib/08_decoret.bib').read)
     refute_nil(bib)
     assert_equal(BibTeX::Bibliography, bib.class)
-    assert_equal(15, bib.data.length)
+    assert_equal(15, bib.length)
     assert_equal([BibTeX::Entry,BibTeX::Comment,BibTeX::String,BibTeX::Preamble], bib.data.map(&:class).uniq)
     assert_equal('py03', bib.data[0].key)
-    assert_equal(:article, bib.data[0].type)
-    assert_equal("{:author=>[\"Xavier D\\\\'ecoret\"], :title=>[\"PyBiTex\"], :year=>[\"2003\"]}", bib.data[0].fields.inspect)
-    assert_equal('key03', bib.data[1].key)
-    assert_equal(:article, bib.data[1].type)
-    assert_equal("{:title=>[\"A {bunch {of} braces {in}} title\"]}", bib.data[1].fields.inspect)
+    assert_equal(:article, bib['py03'].type)
+    assert_equal(['Xavier D\\\'ecoret'], bib['py03'][:author])
+    assert_equal(['PyBiTex'], bib['py03'][:title])
+    assert_equal(['2003'], bib['py03'][:year])
+    assert_equal(:article, bib['key03'].type)
+    assert_equal(['A {bunch {of} braces {in}} title'], bib['key03'][:title])
     #TODO missing assertions
   end
 end
