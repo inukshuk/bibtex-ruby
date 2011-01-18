8f5b13dd066a7d066328d9b448df7a4b787396d2
Sylvester Keil <sylvester@keil.or.at>
2011-01-18 14:26:09 +0100

    Added example bib2html converter

---
+++ examples/bib2html.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'bibtex'
+
+
+# Open a bibliography file
+bib = BibTeX::Bibliography.open(File.expand_path('../markdown.bib',__FILE__),
+  :include => [:meta_comments])
+
+
+# Convert the BibTeX entries into a simple text format
+content = bib.data.map do |d|
+  result = ''
+  
+  if d.class == BibTeX::Entry
+    d.replace!(bib.strings)
+    result = [d[:author], '. ', d[:title], '. ', d[:publisher], ': ', d[:address], ', ', d[:year], '.'].join
+  end
+  
+  if d.class == BibTeX::MetaComment
+    result = d.to_s
+  end
+  
+  result
+end
+
+# Convert all non BibTeX text (i.e., the `meta comments') using the maruku gem
+require 'maruku'
+puts Maruku.new(content.join).to_html_document
