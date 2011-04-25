db8f0e0339d13bb0bbdfaf310e80d9abd6bf2122
Sylvester Keil <sylvester@keil.or.at>
2011-04-25 13:49:03 +0200

    Makes assertion slightly more robust.

--- features/step_definitions/bibtex_steps.rb
+++ features/step_definitions/bibtex_steps.rb
@@ -5,7 +5,7 @@ end
 Then /^my bibliography should contain the following objects:$/ do |table|
   @bibliography.each_with_index do |object, index|
     table.hashes[index].each_pair do |key, value|
-      assert_equal value, object.send(key)
+      assert_equal value, object.send(key).to_s
     end
   end
 end
