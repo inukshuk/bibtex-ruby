0a05fe42baeeeea8440ecd1da140028ff97c800a
Sylvester Keil <sylvester@keil.or.at>
2011-05-12 21:50:06 +0200

    Updates example.

--- examples/bib2html.rb
+++ examples/bib2html.rb
@@ -3,26 +3,36 @@ require 'bibtex'
 
 
 # Open a bibliography file
-bib = BibTeX.open(File.expand_path('../markdown.bib',__FILE__),
-  :include => [:meta_content])
+bib = BibTeX.open File.expand_path('../markdown.bib',__FILE__),
+  :include => [:meta_content]
 
 
-# Convert the BibTeX entries into a simple text format
-content = bib.data.map do |d|
-  result = ''
-  
-  if d.class == BibTeX::Entry
-    d.replace!(bib.strings)
-    result = [d.author, '. ', d.title, '. ', d.publisher, ': ', d.address, ', ', d.year, '.'].join
+# Replaces all strings in the Bibliography and then
+# converts each BibTeX entries to a string using Chicago style
+# (all other elements are mapped to simple strings)
+
+# TODO 1.3.1
+# begin
+#   require 'citeproc'
+# rescue LoadError
+#   puts 'this example depends on the citeproc-ruby gem'
+#   exit
+# end
+
+content = bib.replace.q('@entry, @meta_content').map do |b|
+  if b.entry?  
+    [b.author, '. ', b.title, '. ', b.publisher, ': ', b.address, ', ', b.year, '.'].join
+  else
+    b.to_s
   end
-  
-  if d.class == BibTeX::MetaContent
-    result = d.to_s
-  end
-  
-  result
 end
 
-# Convert all non BibTeX text (i.e., the `meta content') using the maruku gem
-require 'maruku'
-puts Maruku.new(content.join).to_html_document
+
+begin
+  require 'redcarpet'
+rescue LoadError
+  puts 'this example depends on the redcarpet gem'
+  exit
+end
+
+puts Redcarpet.new(content.join).to_html
