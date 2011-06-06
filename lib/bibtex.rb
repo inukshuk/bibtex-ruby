eae7433080fe46ac2f91ea4fdb52ca1a7ae5c5b6
Sylvester Keil <sylvester@keil.or.at>
2011-06-06 16:35:56 +0200

    Removes bundler/setup from main library

--- lib/bibtex.rb
+++ lib/bibtex.rb
@@ -20,7 +20,6 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'rubygems'
-require 'bundler/setup'
 
 require 'bibtex/version'
 require 'logger'
