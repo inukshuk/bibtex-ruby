c8866662902e9fd1a0a0135ae1942e11bb7481ba
Sylvester Keil <sylvester@keil.or.at>
2011-12-19 11:23:23 +0100

    added tests
    #39

--- test/bibtex/test_lexer.rb
+++ test/bibtex/test_lexer.rb
@@ -3,9 +3,19 @@ require 'helper.rb'
 module BibTeX
   class LexerTest < MiniTest::Spec
 
-    it 'should correctly scan a string literal' do
+    it 'correctly scans a string literal' do
       assert_equal Lexer.new.analyse(%q(@string{ x = "foo" })).symbols, [:AT,:STRING,:LBRACE,:NAME,:EQ,:STRING_LITERAL,:RBRACE,false]
     end
- 
+
+    it 'strips line breaks by default' do
+      Lexer.new.analyse(%Q(@string{ x = "foo\nbar" })).stack[-3].must_be :==,
+        [:STRING_LITERAL, 'foo bar']
+    end
+
+    it 'strips whitespace after line breaks by default' do
+      Lexer.new.analyse(%Q(@string{ x = "foo\n    bar" })).stack[-3].must_be :==,
+        [:STRING_LITERAL, 'foo bar']
+    end
+    
   end
 end
