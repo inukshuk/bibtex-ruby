acc3d03036ec30638441cac1f27d692c5fac15bd
Sylvester Keil <sylvester@keil.or.at>
2013-01-13 12:30:58 +0100

    update for 2.2.1 and 2013

--- lib/bibtex.rb
+++ lib/bibtex.rb
@@ -1,6 +1,6 @@
 #--
 # BibTeX-Ruby
-# Copyright (C) 2010-2012	Sylvester Keil <sylvester.keil.or.at>
+# Copyright (C) 2010-2013	Sylvester Keil <sylvester.keil.or.at>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
