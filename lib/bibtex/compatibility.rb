952db5efb0405740e7eec68e57e7b2765d453a49
Christoph Rüßler <christoph.ruessler@gmail.com>
2013-10-10 22:01:41 +0200

    refine transliteration of default_keys add 2 tests

--- lib/bibtex/compatibility.rb
+++ lib/bibtex/compatibility.rb
@@ -16,3 +16,27 @@ if RUBY_VERSION < '1.9'
   
   BibTeX::NameParser.patterns[:upper] = /[[:upper:]ÄÖÜ][^\t\r\n\s\{\}\d\\,]*/o
 end
+
+module BibTeX
+  # Returns true if Iconv is defined.
+  # Tries to load the gem if not. Returns true if successful.
+  def self.iconv_defined?
+    @@uses_iconv ||= defined?(Iconv) || begin
+      require 'iconv'
+    rescue LoadError
+      BibTeX.log.error "Please gem install iconv for Iconv support."
+      false
+    end
+  end
+  
+  # Returns +str+ transliterated containing only ASCII characters.
+  def self.transliterate(str)
+    if self.iconv_defined?
+      @@iconv ||= Iconv.open('ascii//translit//ignore', 'utf-8')
+      @@iconv.iconv(str)
+    else
+      @@replacements ||= Hash[*%w(ä ae ö oe ü ue Ä Ae Ö Oe Ü Ue ß ss)]
+      str.gsub(/[äöüÄÖÜß]/, @@replacements)
+    end
+  end
+end
