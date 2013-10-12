b405b3004b6d09b935c1a10b19620974ddf9355b
Sylvester Keil <sylvester@keil.or.at>
2013-10-12 13:58:25 +0200

    test for icon only once
    see #75

--- lib/bibtex/compatibility.rb
+++ lib/bibtex/compatibility.rb
@@ -18,25 +18,22 @@ if RUBY_VERSION < '1.9'
 end
 
 module BibTeX
-  # Returns true if Iconv is defined.
-  # Tries to load the gem if not. Returns true if successful.
-  def self.iconv_defined?
-    @@uses_iconv ||= defined?(Iconv) || begin
-      require 'iconv'
-    rescue LoadError
-      BibTeX.log.error "Please gem install iconv for Iconv support."
-      false
+  begin
+    require 'iconv'
+
+    @iconv = Iconv.open('ascii//translit//ignore', 'utf-8')
+
+    def self.transliterate(str)
+      @iconv.iconv(str)
+    end
+  rescue LoadError
+
+    @iconv_replacements = Hash[*%w(ä ae ö oe ü ue Ä Ae Ö Oe Ü Ue ß ss)]
+
+    # Returns +str+ transliterated containing only ASCII characters.
+    def self.transliterate(str)
+      str.gsub(/[äöüÄÖÜß]/, @iconv_replacements)
     end
   end
   
-  # Returns +str+ transliterated containing only ASCII characters.
-  def self.transliterate(str)
-    if self.iconv_defined?
-      @@iconv ||= Iconv.open('ascii//translit//ignore', 'utf-8')
-      @@iconv.iconv(str)
-    else
-      @@replacements ||= Hash[*%w(ä ae ö oe ü ue Ä Ae Ö Oe Ü Ue ß ss)]
-      str.gsub(/[äöüÄÖÜß]/, @@replacements)
-    end
-  end
 end
