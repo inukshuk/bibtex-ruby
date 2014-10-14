58332776988b705612be07359e4fa4697cf82d20
Sylvester Keil <sylvester@keil.or.at>
2014-10-14 10:48:02 +0200

    update examples
    see #97

--- examples/bib2html.rb
+++ examples/bib2html.rb
@@ -1,28 +1,33 @@
 require 'rubygems'
 require 'bibtex'
 
+begin
+  require 'citeproc/ruby'
+  require 'csl/styles'
+rescue LoadError
+  puts 'this example depends on the citeproc-ruby and csl-styles gems'
+  exit
+end
 
 # Open a bibliography file
 bib = BibTeX.open File.expand_path('../markdown.bib',__FILE__),
   :include => [:meta_content]
 
-
 # Replaces all strings in the Bibliography and then
 # converts each BibTeX entries to a string using Chicago style
 # (all other elements are mapped to simple strings)
+bib.replace
 
-begin
-  require 'citeproc'
-rescue LoadError
-  puts 'this example depends on the citeproc-ruby gem'
-  exit
-end
+cp = CiteProc::Processor.new :style => 'apa',
+  :format => 'html', :locale => 'en'
 
-content = bib.replace.q('@entry, @meta_content').map do |b|
-  if b.entry?  
-    CiteProc.process b.to_citeproc
+cp.import bib.to_citeproc
+
+content = bib['@entry, @meta_content'].map do |e|
+  if e.entry?
+    cp.render :bibliography, :id => e.key
   else
-    b.to_s
+    e.to_s
   end
 end
 
