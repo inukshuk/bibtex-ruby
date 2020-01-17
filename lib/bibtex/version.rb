3a3224201aed65e9ccbc2d2e8bbaec3c6789ed6f
Sylvester Keil <sylvester@keil.or.at>
2020-01-17 15:54:34 +0100

    5.1.0

--- lib/bibtex/version.rb
+++ lib/bibtex/version.rb
@@ -19,8 +19,8 @@
 module BibTeX
   module Version
     MAJOR = 5
-    MINOR = 0
-    PATCH = 1
+    MINOR = 1
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').freeze
