750d8a05df403c3beff90048e771882dcfba7108
Sylvester Keil <sylvester@keil.or.at>
2025-07-20 13:24:17 +0200

    Update test syntax

--- test/bibtex/test_lexer.rb
+++ test/bibtex/test_lexer.rb
@@ -7,33 +7,38 @@ module BibTeX
     end
 
     it 'strips line breaks by default' do
-      Lexer.new.analyse(%(@string{ x = "foo\nbar" })).stack[-3].must_be :==,
-                                                                        [:STRING_LITERAL, 'foo bar']
+      assert_equal Lexer.new.analyse(%(@string{ x = "foo\nbar" })).stack[-3],
+        [:STRING_LITERAL, 'foo bar']
     end
 
     it 'strips whitespace after line breaks by default' do
-      Lexer.new.analyse(%(@string{ x = "foo\n    bar" })).stack[-3].must_be :==,
-                                                                            [:STRING_LITERAL, 'foo bar']
+      assert_equal Lexer.new.analyse(%(@string{ x = "foo\n    bar" })).stack[-3],
+        [:STRING_LITERAL, 'foo bar']
     end
 
     it 'matches KEY tokens' do
-      Lexer.new.analyse('@misc{foo, }').symbols.must_be :==, [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
+      assert_equal Lexer.new.analyse('@misc{foo, }').symbols,
+        [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
     end
 
     it 'matches KEY tokens with non-ascii characters' do
-      Lexer.new.analyse('@misc{löwe, }').symbols.must_be :==, [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
+      assert_equal Lexer.new.analyse('@misc{löwe, }').symbols,
+        [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
     end
 
     it 'matches KEY tokens after whitespace' do
-      Lexer.new.analyse('@misc{  foo, }').symbols.must_be :==, [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
+      assert_equal Lexer.new.analyse('@misc{  foo, }').symbols,
+        [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
     end
 
     it "doesn't start a comment for types starting with but not equal @comment" do
-      Lexer.new.analyse('@commentary{staudinger, }').symbols.must_be :==, [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
+      assert_equal Lexer.new.analyse('@commentary{staudinger, }').symbols,
+        [:AT, :NAME, :LBRACE, :KEY, :RBRACE, false]
     end
 
     it "doesn't start a preamble for types starting with but not equal @preamble" do
-      Lexer.new.analyse('@preamblestring{ preamble }').symbols.must_be :==, [:AT, :NAME, :LBRACE, :NAME, :RBRACE, false]
+      assert_equal Lexer.new.analyse('@preamblestring{ preamble }').symbols,
+        [:AT, :NAME, :LBRACE, :NAME, :RBRACE, false]
     end
   end
 end
