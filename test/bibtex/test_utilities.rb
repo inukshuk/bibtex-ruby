f85009d4ae0abe3620e1c249636188757534169a
Sylvester Keil <sylvester@keil.or.at>
2025-07-20 13:01:38 +0200

    Fix test class name

--- test/bibtex/test_utilities.rb
+++ test/bibtex/test_utilities.rb
@@ -1,7 +1,7 @@
 require 'helper.rb'
 
 module BibTeX
-  class TestBibtex < Minitest::Test
+  class TestUtilities < Minitest::Test
     def test_empty?
       assert BibTeX.open(Test.fixtures(:empty)).empty?
       refute BibTeX.open(Test.fixtures(:bibdesk)).empty?
